a9547d9db33fb77e817469e3868c817e118a75d0
Stephen Baynham <sdbaynham@gmail.com>
2022-12-26 15:06:42 -0800

    Expose MaxMemoryTypes/MaxMemoryHeaps consts

---
+++ common/const.go
@@ -0,0 +1,12 @@
+package common
+
+/*
+#include <stdlib.h>
+#include "vulkan.h"
+*/
+import "C"
+
+const (
+	MaxMemoryTypes int = C.VK_MAX_MEMORY_TYPES
+	MaxMemoryHeaps int = C.VK_MAX_MEMORY_HEAPS
+)
