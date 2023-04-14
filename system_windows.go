c64bc1b21b22ab92a8ec21a4bb44b66f5e9f923c
Stephen Baynham <sdbaynham@gmail.com>
2023-04-14 14:24:15 -0700

    Allow reusing procaddr previously loaded

--- system_windows.go
+++ system_windows.go
@@ -8,20 +8,35 @@ import (
 	"unsafe"
 )
 
+var vulkanDLL syscall.Handle
+var getInstanceProcAddr unsafe.Pointer
+
+func loadProcAddr() error {
+	var err error
+	vulkanDLL, err = syscall.LoadLibrary("vulkan-1.dll")
+	if err != nil {
+		return err
+	}
+
+	getInstanceProcAddrHandle, err := syscall.GetProcAddress(vulkanDLL, "vkGetInstanceProcAddr")
+	if err != nil {
+		return err
+	}
+	getInstanceProcAddr = unsafe.Pointer(getInstanceProcAddrHandle)
+
+	return nil
+}
+
 // CreateSystemLoader generates a Loader from a vulkan-1.dll/so located on the local file system
 //
 // Allowing cgo to bring us the vkGetInstanceProcAddr method on windows, for whatever reason, causes heap corruption
 // when the garbage collector runs. For whatever reason, manually loading it from dll does not have this issue
 func CreateSystemLoader() (*VulkanLoader, error) {
-	vulkan, err := syscall.LoadLibrary("vulkan-1.dll")
-	if err != nil {
-		return nil, err
+	if getInstanceProcAddr == nil {
+		err := loadProcAddr()
+		if err != nil {
+			return nil, err
+		}
 	}
-
-	getInstanceProcAddr, err := syscall.GetProcAddress(vulkan, "vkGetInstanceProcAddr")
-	if err != nil {
-		return nil, err
-	}
-
-	return CreateLoaderFromProcAddr(unsafe.Pointer(getInstanceProcAddr))
+	return CreateLoaderFromProcAddr(getInstanceProcAddr)
 }
